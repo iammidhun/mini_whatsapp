43f90acc20c865cd33ff285e8e526a01e0bc57e6
iammidhun <midhunmohanputhur@gmail.com>
2019-12-11 15:35:59 +0530

    debug = True

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 def hello_world():
 	return 'hi'
 
-if __name__ == "__main__":
-	app.run()
-	
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['MONGODB_SETTINGS'] = {
 	'db': 'whatsapp',
 	'host': 'localhost',
 	'port': 27017
 }
+if __name__ == "__main__":
+	app.run(debug=True)
+	
